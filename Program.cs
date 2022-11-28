d1242733b3c23ca88cbb0811e448f44e56466470
Mhd Ali Houria <alihoria123456@gmail.com>
2022-11-28 12:45:11 +0200

    Strings and C# string methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
-﻿int input = 5;
-int num = 16;
-int secondNum = 10;
-input += num + secondNum;
-Console.WriteLine ( input);
+﻿//int input = 5;
+//int num = 16;
+//int secondNum = 10;
+//input += num + secondNum;
+//------------------------------------------------------------
+//Random random = new Random();
+//int randomNumber = random.Next(1,6) + 1;
+//------------------------------------------------------------
+//calculating the hypotenuse of a triangle 
+//Console.WriteLine("Enter side A");
+//double a = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Enter side B");
+//double b = Convert.ToDouble(Console.ReadLine());
+
+//double c = Math.Sqrt(a * a + b * b);
+
+//Console.WriteLine("Side C is " + c);
+//------------------------------------------------------------
+//String methods
+//string fullName = "Mhd Ali Houria";
+//string phoneNumber = "123-456-7890";
+
+//phoneNumber = phoneNumber.Replace("-", " ");
+//Console.WriteLine(phoneNumber);
+
+//fullName = fullName.ToLower();
+//fullName = fullName.ToUpper();
+//fullName = fullName.Insert(0, "Mr.");
+//Console.WriteLine(fullName.Length);
+
+//string firstName = fullName.Substring(0, 7);
+//string lastName = fullName.Substring(8);
+//------------------------------------------------------------
